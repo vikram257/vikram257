f6fe9dcec39bbb4398c0060b94b6fb25832035ea
vikram257 <88422318+vikram257@users.noreply.github.com>
2023-07-21 18:26:18 +0530

    Add files via upload

---
+++ CICD.bat
@@ -0,0 +1,18 @@
+ipconfig /all
+
+ping google.com
+               
+nslookup google.com                  
+                    
+netstat -anp
+
+systeminfo
+
+getmac     
+
+PAUSE  
+                    
+                    
+                    
+                    
+                   
